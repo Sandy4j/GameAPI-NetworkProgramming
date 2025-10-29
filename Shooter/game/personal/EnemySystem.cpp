082f7e126dd52a778f978d742c4bdcfdc27ec1f9
aditya <adityamuhammad600@gmail.com>
2025-10-30 02:49:55 +0700

    FIX: Cleaned .vs and Debug files from tracking

--- Shooter/game/personal/EnemySystem.cpp
+++ Shooter/game/personal/EnemySystem.cpp
@@ -1,75 +1,143 @@
-//#include <ctime>
-//#include <random>
 #include <glm/glm.hpp>
+#include <algorithm>
+#include <random>
+#include <chrono>
 
 #include "TimeManager.h"
-
 #include "EnemySystem.h"
+
+EnemySystem::EnemySystem(Transform* temp_transform)
+    : transform(temp_transform),
+    m_currentState(MovementState::Idle),
+    m_hasTarget(false),
+    m_jumpElapsedTime(0.0f),
+    m_moveSpeed(0.5f),
+    m_decisionTimer(0.0f),
+    m_idleTimer(0.0f),
+    m_minBounds(-1.0f, -1.0f),
+    m_maxBounds(1.0f, 1.0f),
+    m_fixedZ(-1.5f),          
+    m_rng(std::chrono::high_resolution_clock::now().time_since_epoch().count()),
+    m_distFloat(0.0f, 1.0f),
+    m_distDecision(1, 100)
+{
+}
 
 void EnemySystem::Start()
 {
-    /*std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> distrib(-.5f, .5f);
+    float startX = m_minBounds.x + (m_maxBounds.x - m_minBounds.x) * m_distFloat(m_rng);
+    transform->position = glm::vec3(startX, -1.4, m_fixedZ);
 
-    float random_float = distrib(gen);
+    m_decisionTimer = 1.0f + 2.0f * m_distFloat(m_rng);
+}
 
-	transform->position = glm::vec3(random_float, random_float, 0);
+glm::vec3 EnemySystem::CalculateBezierPoint(float t, const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2)
+{
+    float u = 1.0f - t;
+    float tt = t * t;
+    float uu = u * u;
+    glm::vec3 p = uu * p0 + 2.0f * u * t * p1 + tt * p2;
+    return p;
+}
 
-    target = (random_float > 0)? -.5f : .5f;
-    dir = (target > 0) ? -1 : 1;*/
+void EnemySystem::InitiateJump()
+{
+    m_currentState = MovementState::Jumping;
+    m_jumpStartPos = transform->position;
 
-    target_position[0] = glm::vec3(-.5, 0, 0);
-    target_position[1] = glm::vec3(-.25, .35, 0);
-    target_position[2] = glm::vec3(0, .5, 0);
-    target_position[3] = glm::vec3(.25, .35, 0);
-    target_position[4] = glm::vec3(.5, 0, 0);
+    float endX = m_minBounds.x + (m_maxBounds.x - m_minBounds.x) * m_distFloat(m_rng);
+    m_jumpEndPos = glm::vec3(endX, m_jumpStartPos.y, m_fixedZ);
 
-    index = 0;
-    dir = 1;
+    m_jumpControlPos = (m_jumpStartPos + m_jumpEndPos) / 2.0f;
+    float randomHeight = 1.5f + 2.0f * m_distFloat(m_rng);
+    m_jumpControlPos.y = m_jumpStartPos.y + randomHeight; 
+    m_jumpControlPos.z = m_fixedZ;
+
+    m_jumpDuration = 1.0f + 1.0f * m_distFloat(m_rng);
+    m_jumpElapsedTime = 0.0f;
+}
+
+void EnemySystem::PickNewTarget()
+{
+    float targetX = m_minBounds.x + (m_maxBounds.x - m_minBounds.x) * m_distFloat(m_rng);
+    float targetY = transform->position.y;
+    m_targetPos = glm::vec3(targetX, targetY, m_fixedZ);
+    m_hasTarget = true;
+    m_currentState = MovementState::Moving;
 }
 
 void EnemySystem::Update()
 {
-    glm::vec3 currentPos = transform->position;
-    glm::vec3 targetPos = target_position[index];
+    float deltaTime = TimeManager::GetInstance().GetDeltaTime();
 
-    glm::vec3 moveDirection = targetPos - currentPos;
-    float distanceToTarget = glm::length(moveDirection);
-
-    const float threshold = 0.01f;
-    if (distanceToTarget < threshold)
+    if (m_decisionTimer > 0.0f) 
     {
-        transform->position = targetPos;
-
-        int nextIndex = index + dir;
-
-        if (nextIndex >= 5)
-        {
-            dir = -1;
-            index = 0;
-        }
-        else if (nextIndex < 0)
-        {
-            dir = 1;
-            index = 0;
-        }
-        else
-        {
-            index = nextIndex;
-        }
+        m_decisionTimer -= deltaTime;
     }
-    else
+
+    switch (m_currentState)
     {
-        moveDirection = glm::normalize(moveDirection);
-
-        float moveAmount = .75 * TimeManager::GetInstance().GetDeltaTime();
-
-        if (moveAmount > distanceToTarget) 
+    case MovementState::Idle:
+        m_idleTimer -= deltaTime;
+        if (m_idleTimer <= 0.0f) 
         {
-            moveAmount = distanceToTarget;
+            int decisionRoll = m_distDecision(m_rng);
+            if (decisionRoll <= 70) 
+                PickNewTarget();
+            else
+                InitiateJump();
         }
+        break;
+    case MovementState::Moving:
+        if (!m_hasTarget)
+            PickNewTarget();
 
-        transform->position += moveDirection * moveAmount;
+        glm::vec3 currentPos = transform->position;
+        glm::vec3 targetPos = m_targetPos;
+        glm::vec3 moveDirection = targetPos - currentPos;
+        float distanceToTarget = glm::length(glm::vec2(moveDirection.x, moveDirection.y));
+
+        const float threshold = 0.05f;
+        if (distanceToTarget < threshold) 
+        {
+            transform->position = targetPos;
+            m_hasTarget = false;
+            m_idleTimer = 0.5f + 1.0f * m_distFloat(m_rng);
+            m_currentState = MovementState::Idle;
+        }
+        else 
+        {
+            moveDirection = glm::normalize(moveDirection);
+            float moveAmount = m_moveSpeed * deltaTime;
+            if (moveAmount > distanceToTarget) moveAmount = distanceToTarget;
+            transform->position += moveDirection * moveAmount;
+
+            if (m_decisionTimer <= 0.0f) 
+            {
+                int decisionRoll = m_distDecision(m_rng);
+                if (decisionRoll > 90) 
+                {
+                    InitiateJump();
+                    m_decisionTimer = 2.0f + 3.0f * m_distFloat(m_rng);
+                }
+                else
+                    m_decisionTimer = 0.5f + 1.5f * m_distFloat(m_rng);
+            }
+        }
+        break;
+    case MovementState::Jumping:
+        m_jumpElapsedTime += deltaTime;
+        float t = std::min(1.0f, m_jumpElapsedTime / m_jumpDuration);
+
+        transform->position = CalculateBezierPoint(t, m_jumpStartPos, m_jumpControlPos, m_jumpEndPos);
+        transform->position.z = m_fixedZ;
+
+        if (t >= 1.0f) 
+        {
+            transform->position.y = m_jumpEndPos.y;
+            m_idleTimer = 0.5f + 1.0f * m_distFloat(m_rng);
+            m_currentState = MovementState::Idle;
+        }
+        break;
     }
 }
--- Shooter/game/personal/EnemySystem.h
+++ Shooter/game/personal/EnemySystem.h
@@ -1,27 +1,58 @@
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 #ifndef ENEMYSYSTEM_H
 #define ENEMYSYSTEM_H
 
-#include "glm/glm.hpp"
+#include <glm/glm.hpp>
+#include <random>
 
 #include "Transform.h"
-#include "Personal.h"
 
 class EnemySystem
 {
 public:
-	EnemySystem(Transform* temp_transform) : transform(temp_transform) {};
-	~EnemySystem() = default;
+    EnemySystem(Transform* temp_transform);
+    ~EnemySystem() = default;
 
 public:
-	void Start();
-	void Update();
+    void Start();
+    void Update();
 
 private:
-	glm::vec3 target_position[5];
-	Transform* transform;
-	float target;
-	int index;
-	int dir;
+    glm::vec3 CalculateBezierPoint(float t, const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2);
+    void PickNewTarget();
+    void InitiateJump();
+
+    enum class MovementState {
+        Idle,
+        Moving,
+        Jumping
+    };
+    MovementState m_currentState;
+
+    glm::vec3 m_targetPos;
+    bool m_hasTarget;
+
+    glm::vec3 m_jumpStartPos;
+    glm::vec3 m_jumpControlPos;
+    glm::vec3 m_jumpEndPos;
+    float m_jumpDuration;
+    float m_jumpElapsedTime;
+
+    float m_decisionTimer;
+    float m_idleTimer;
+
+    float m_moveSpeed;
+
+    glm::vec2 m_minBounds;
+    glm::vec2 m_maxBounds;
+    float m_fixedZ;       
+
+    std::mt19937 m_rng;
+    std::uniform_real_distribution<float> m_distFloat;
+    std::uniform_int_distribution<int> m_distDecision;
+
+    Transform* transform;
 };
 
 #endif
