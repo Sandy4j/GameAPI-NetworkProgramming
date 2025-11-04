ed63d79ec792ecfa4402244d8b980a4b1aa22ccb
aditya <adityamuhammad600@gmail.com>
2025-11-04 14:05:06 +0700

    update-- ignore--adit

--- Shooter/game/personal/awan/AwanSystem.cpp
+++ Shooter/game/personal/awan/AwanSystem.cpp
@@ -14,7 +14,7 @@ AwanSystem::AwanSystem(Transform* temp_transform) :
     float scale = RandomNumber(.15, .2);
     transform->scale = glm::vec3(scale, scale, 0);
 
-    transform->position.x = -1;
+    //transform->position.x = -1;
 }
 
 float AwanSystem::RandomNumber(float min, float max)
@@ -28,17 +28,17 @@ float AwanSystem::RandomNumber(float min, float max)
 
 void AwanSystem::IAwanStart()
 {
-
+    transform->position = glm::vec3(0, 0, 0);
 }
 
 void AwanSystem::IAwanUpdate()
 {
-    float deltatime = TimeManager::GetInstance().GetDeltaTime();
+    float deltatime = TimeManager::GetInstance().GetDeltaTime() * .1;
     y_position += deltatime;
     float sin = std::sin(y_position);
-    sin /= 10;
+    //sin /= 10;
     glm::vec3 pos = transform->position;
-    pos.x += deltatime;
+    pos.x += deltatime * 10;
     pos.y += sin;
     pos *= .1;
     transform->position = glm::vec3(pos);
